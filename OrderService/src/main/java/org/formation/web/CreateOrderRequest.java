93de83efb7bc723ac7e1e257ff9973c039e2497d
David THIBAU <david.thibau@gmail.com>
2021-10-01 16:36:33 +0200

    Services Registered in Eureka

--- OrderService/src/main/java/org/formation/web/CreateOrderRequest.java
+++ OrderService/src/main/java/org/formation/web/CreateOrderRequest.java
@@ -5,15 +5,14 @@ import java.util.List;
 
 import org.formation.domain.Address;
 import org.formation.domain.OrderItem;
+import org.formation.domain.PaymentInformation;
 
 import lombok.Data;
 
 @Data
 public class CreateOrderRequest {
 
-	  private long restaurantId;
-	  private long consumerId;
-	  private LocalDateTime deliveryTime;
 	  private List<OrderItem> lineItems;
 	  private Address deliveryAddress;
+	  private PaymentInformation paymentInformation;
 }
