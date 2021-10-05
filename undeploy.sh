0f95f7931643cb4c5b2b99a0c2282e87029f479e
David THIBAU <david.thibau@gmail.com>
2021-10-05 22:04:59 +0200

    Déploiement stack sur kubernetes

---
+++ undeploy.sh
@@ -0,0 +1,11 @@
+kubectl delete deployment config
+kubectl delete deployment eureka
+kubectl delete deployment livraison-service
+kubectl delete deployment order-service
+kubectl delete deployment order-query-service
+kubectl delete deployment payment-service
+kubectl delete deployment product-service
+kubectl delete deployment gateway
+
+
+
