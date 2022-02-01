a6d51453923e57dbb2297d7e7ac17fce4c5f7f9a
David THI <david.thibau@gmail.com>
2022-02-01 16:28:23 +0100

    Transactional services

--- ProductService/src/main/java/org/formation/service/EventService.java
+++ ProductService/src/main/java/org/formation/service/EventService.java
@@ -3,8 +3,10 @@ package org.formation.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class EventService {
 
 	private static String TICKET_CHANNEL="tickets";
