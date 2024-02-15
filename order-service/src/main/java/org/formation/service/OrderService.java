1d9790c43e505ae0ea98802917f9c506ab0b6002
David THIBAU <david.thibau@gmail.com>
2024-02-15 17:29:18 +0100

    CircuitBreaker

--- order-service/src/main/java/org/formation/service/OrderService.java
+++ order-service/src/main/java/org/formation/service/OrderService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.formation.domain.Order;
 import org.formation.domain.repository.OrderRepository;
 import org.formation.web.CreateOrderRequest;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -16,31 +17,40 @@ import lombok.extern.java.Log;
 @Log
 public class OrderService {
 
-	
 	private final RestTemplate productRestTemplate;
 
 	private final OrderRepository orderRepository;
-	
-	public OrderService(OrderRepository orderRepository, RestTemplate productRestTemplate ) {
+
+	private CircuitBreakerFactory cbFactory;
+
+	public OrderService(OrderRepository orderRepository, RestTemplate productRestTemplate,
+			CircuitBreakerFactory cbFactory) {
 		this.orderRepository = orderRepository;
 		this.productRestTemplate = productRestTemplate;
+		this.cbFactory = cbFactory;
 	}
 
-
 	public Order createOrder(CreateOrderRequest createOrderRequest) {
-			
-			// Save in local DataBase
-			Order order = orderRepository.save(createOrderRequest.getOrder());
-			
-			// Create Ticket in ProductService
-			List<ProductRequest> productRequest = order.getOrderItems().stream().map(i -> new ProductRequest(i)).toList();
-			
-			String endPoint = "/api/tickets/{oderId}";
-			
-			Ticket t = productRestTemplate.postForObject(endPoint, productRequest, Ticket.class, order.getId());
-			
-			log.info("Ticket created " + t);
-			
-			return order;
-		}
+
+		// Save in local DataBase
+		Order order = orderRepository.save(createOrderRequest.getOrder());
+
+		Ticket t = _createTicket(order);
+		
+		log.info("Ticket created " + t);
+
+		return order;
+	}
+
+	private Ticket _createTicket(Order order) {
+		List<ProductRequest> productRequest = order.getOrderItems().stream().map(i -> new ProductRequest(i)).toList();
+		String endPoint = "/api/tickets/{oderId}";
+
+		return cbFactory.create("createTicket").run(
+				() -> productRestTemplate.postForObject(endPoint, productRequest, Ticket.class, order.getId()), t -> {
+					log.warning("FALLBACK " + t);
+					return null;
+				});
+
+	}
 }
