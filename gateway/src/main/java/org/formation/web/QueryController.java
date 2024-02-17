59973705a7ff86d7231737f9460f33fea14728a1
David THIBAU <david.thibau@gmail.com>
2024-02-17 23:54:03 +0100

    Routes de la gateway

--- gateway/src/main/java/org/formation/web/QueryController.java
+++ gateway/src/main/java/org/formation/web/QueryController.java
@@ -13,7 +13,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/orders/query")
 public class QueryController {
 
 	
