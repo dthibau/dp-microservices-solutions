8baed84cdf1e0fa632ada95b721a870eb55e7213
David THIBAU <david.thibau@gmail.com>
2024-02-18 02:39:44 +0100

    Conversion groups vers rôle

--- gateway/src/main/java/org/formation/SecurityConfig.java
+++ gateway/src/main/java/org/formation/SecurityConfig.java
@@ -2,8 +2,10 @@ package org.formation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -11,9 +13,20 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityWebFilterChain securitygWebFilterChain(	ServerHttpSecurity http) {
-		return http.authorizeExchange(acl -> acl.anyExchange().authenticated())
-				.oauth2ResourceServer((v -> v.jwt(Customizer.withDefaults())))
+		return http.authorizeExchange(acl -> acl.anyExchange().hasAnyRole("MANAGER","CLIENT"))
+				.oauth2ResourceServer((v -> v.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))))
 				.csrf(csrf -> csrf.disable())
 				.build();
+	}
+	
+	@Bean
+	public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+	    grantedAuthoritiesConverter.setAuthoritiesClaimName("groups");
+	    grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+	    ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+	    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+	    return jwtAuthenticationConverter;
 	}
 }
