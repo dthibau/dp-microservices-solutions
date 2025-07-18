660e38428eaf1569d38489208e8be73cacc18f52
David THIBAU <david.thibau@gmail.com>
2025-07-18 16:30:28 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@ kubectl apply -f k8s/config.yml
 kubectl apply -f k8s/eureka.yml
 kubectl apply -f k8s/livraison-service.yml
 kubectl apply -f k8s/order-service.yml
-kubectl apply -f k8s/order-query-service.yml
 kubectl apply -f k8s/payment-service.yml
 kubectl apply -f k8s/product-service.yml
 kubectl apply -f k8s/gateway.yml
